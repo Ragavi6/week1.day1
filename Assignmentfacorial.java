d1d23661247f438b0f8d08b222d874b69dfe7bd5
Ragavi6 <94974384+Ragavi6@users.noreply.github.com>
2021-11-28 13:00:31 +0530

    Add files via upload

---
+++ Assignmentfacorial.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Assignmentfacorial {
+
+	public static void main(String[] args) {
+		
+		int i=5;
+		int fact=1;
+		 for (int r=i; r>0; r--)
+		 {
+			 fact=fact*r;
+	
+		 }
+		 System.out.println(fact);
+	}
+		
+	}
---
+++ Assignmentfionacciseries.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Assignmentfionacciseries {
+
+	public static void main(String[] args) {
+		
+		int n3,range=8;
+		int n1=0;
+		int n2=1;
+	
+			System.out.println(n1);
+			
+			for(n3=1;n3<=8;n3++)
+		
+			{
+				range=n1+n2;
+				System.out.println(range);
+				n1=n2;
+				n2=range;
+				
+			}
+	}
+
+
+}
